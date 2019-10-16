a50474cc4a74454488260efcdbcda18249c1d47d
gregg1494 <gregg1494@gmail.com>
2019-10-16 00:15:19 -0500

    sequelize is connecting to aws, added some middleware

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
 const express = require('express');
+const Sequelize = require('sequelize');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+require('dotenv').config();
+
+// DB connection
+let sequelize = new Sequelize(process.env.DB_CONNECTIONSTRING);
+
+// Check DB connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 let app = express();
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Express messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Passport config
+require('./config/passport')(passport);
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('ci with travis');
 });
+
 
 let server = app.listen(3000, () => {
   console.log('App running on port 3000');
