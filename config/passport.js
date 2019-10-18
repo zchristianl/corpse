50831ab16d028bbc20071eedda687d0c9d50d812
gregg1494@gmail.com <gregg1494@gmail.com>
2019-10-18 03:39:09 -0700

    removed comments

--- config/passport.js
+++ config/passport.js
@@ -29,33 +29,6 @@ module.exports = (passport) => {
     }
   ));
 
-
-
-
-  // passport.use(new LocalStrategy( 
-  //   (email, password, done) => {
-  //     logger.debug('in here');
-  //     User.findOne({where: {'email': email}}).then((user) => {
-  //       logger.debug('in here');
-  //       if (!user) {
-  //         return done(null, false, { message: 'Email does not exist' });
-  //       }
-
-  //       bcrypt.compare(password, user.password, (err, isMatch) => {
-  //         if(err) throw err;
-  //         if(isMatch){
-  //           return done(null, user);
-  //         } else {
-  //           return done(null, false, {message: "Invalid username or password"});
-  //         }
-  //       });
-  //     }).catch((err) => {
-  //       console.log("Error:",err);
-  //       return done(null, false, { message: 'Something went wrong with your login' });
-  //     });
-  //   }
-  // ));
-
   passport.serializeUser(function(user, done) {
     done(null, user.id);
   });
