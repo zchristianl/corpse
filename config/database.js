038b3b222b854cf0361a68871b23557149baeb34
zchristianl <6568643+zchristianl@users.noreply.github.com>
2019-10-20 23:35:20 -0500

    Inventory Model

--- config/database.js
+++ config/database.js
@@ -1,20 +1,49 @@
 const Sequelize = require('sequelize');
-const UserModel = require('../models/user');
+
+
+
+const ProjectRequire = {};
 const logger = require('../utils/logger');
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+
+
+
+/*
+* Register all models into ProjectRequire based on model name
+* */
+
+let files =fs.readdirSync(path.join(__dirname,'../models'));
+
+files.forEach((f)=> {
+  if (path.extname(f) === '.js') {
+    let fileName = path.basename(f, '.js');
+    ProjectRequire[fileName] = require('../models/' + fileName + '.js');
+  }
+});
 
 // DB connection
 const db = new Sequelize(process.env.DB_CONNECTIONSTRING);
+/*
+ * Model Registration Step 1
+ * Declare variable and call (db, Sequelize) on it based on ProjectRequire
+*/
+const User = ProjectRequire.user(db, Sequelize);
+const Department = ProjectRequire.department(db, Sequelize);
+const Inventory = ProjectRequire.Inventory(db, Sequelize);
 
-const User = UserModel(db, Sequelize);
+db.sync()
+  .then(() => {
+    logger.info('Database & tables created!');
+  });
 
-db.sync().then(function() {
-  logger.info('Nice! Database looks fine.');
-}).catch(function(err) {
-  logger.error(err, 'Something went wrong with the Database Update!');
-});
 
+
+//Model Registration Step 2, Add variable to this list
 module.exports = {
   db,
-  User
+  User,
+  Department,
+  Inventory
 };
