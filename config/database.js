50129c5fbc2b79b05c1f34b285a7b8c410ce5583
zchristianl <6568643+zchristianl@users.noreply.github.com>
2019-10-23 16:12:58 -0500

    Add model to database.js plus Inventory stuff

--- config/database.js
+++ config/database.js
@@ -1,14 +1,9 @@
 const Sequelize = require('sequelize');
-
-
-
 const ProjectRequire = {};
 const logger = require('../utils/logger');
 require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
-
-
 
 /*
 * Register all models into ProjectRequire based on model name
@@ -30,20 +25,17 @@ const db = new Sequelize(process.env.DB_CONNECTIONSTRING);
  * Declare variable and call (db, Sequelize) on it based on ProjectRequire
 */
 const User = ProjectRequire.user(db, Sequelize);
-const Department = ProjectRequire.department(db, Sequelize);
-const Inventory = ProjectRequire.Inventory(db, Sequelize);
+const Inventory = ProjectRequire.inventory(db, Sequelize);
 
-db.sync()
-  .then(() => {
-    logger.info('Database & tables created!');
-  });
-
-
+db.sync().then(function() {
+  logger.info('Database tables synced. Oh Joy!');
+}).catch(function(err) {
+  logger.error(err, 'Something went wrong with the Database Update!');
+});
 
 //Model Registration Step 2, Add variable to this list
 module.exports = {
   db,
   User,
-  Department,
   Inventory
 };
