fedfd46d2827ba402496071f8d38053ad7bc7876
gregg1494 <gregg1494@gmail.com>
2019-10-25 22:43:44 -0500

    fix build error, update insitution and user model

--- models/user.js
+++ models/user.js
@@ -4,15 +4,13 @@ module.exports = (sequelize, type) => sequelize.define('user', {
     primaryKey: true,
     autoIncrement: true
   },
-  name: type.STRING,
-  username: type.STRING,
+  first_name: type.STRING,
+  last_name: type.STRING,
   email: type.STRING,
   password: type.STRING,
-  first: type.STRING,
-  last: type.STRING,
-  email: type.STRING,
-  title: type.STRING(10), //10
-  phone: type.STRING(12) //12
+  account: type.STRING(10), //12
+  phone: type.STRING(12), //12
+  fax: type.STRING(12), //12
   //Foreign Keys
   //dept, lab, location, linked user????
 });
