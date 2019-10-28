297d7a440bca732341e18aa1337ef0390ae890ad
Ryan Castle <rcastle@wisc.edu>
2019-10-28 18:33:00 -0500

    testing travis ci automated testing

--- test/app-spec.js
+++ test/app-spec.js
@@ -5,3 +5,42 @@ describe('test', () => {
     expect('ci with travis').to.equal('ci with travis');
   });
 });
+
+var User, app, request, server, should, user, agent;
+
+should   = require("should");
+app      = require("../server");
+user     = model("user");
+request  = require("supertest");
+agent = request.agent(app);
+
+describe('User', function () {
+  before(function(done) {
+      user = new user({
+        email    : "user@user.com",
+        first_name: "Full Name",
+        last_name : "Last Name",
+        password : "pass11",
+        account_type : "account_type",
+        phone: "phone_number",
+        fax: "fax number"
+      });
+      user.save(done)
+    });
+  describe('Login test', function () {
+      it('should redirect to /', function (done) {
+        agent
+        .post('/users/session')
+        .field('email', 'user@user.com')
+        .field('password', 'pass11')
+        .expect('Location','/')
+        .end(done)
+      })
+
+  after(function(done) {
+      User.remove().exec();
+      return done();
+    });
+
+})
+})
