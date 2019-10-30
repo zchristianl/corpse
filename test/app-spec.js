92a7acd4508cae636e2d3d2be161053e6a80b5ca
gregg1494@gmail.com <gregg1494@gmail.com>
2019-10-30 16:59:05 -0700

    travis error fix?

--- test/app-spec.js
+++ test/app-spec.js
@@ -9,6 +9,14 @@ describe('test', () => {
   it('should return a string', () => {
     chai.expect('ci with travis').to.equal('ci with travis');
   });
-
-
+  
+  it('/user/login GET', function(done) {
+    this.timeout(0);
+    chai.request(app)
+      .get('/users/login')
+      .end(function(err, res){
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
 });
