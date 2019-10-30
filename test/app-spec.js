49502950b6c821eb7739c2ec0229277cf85dd4e2
gregg1494@gmail.com <gregg1494@gmail.com>
2019-10-30 16:47:40 -0700

    remove test

--- test/app-spec.js
+++ test/app-spec.js
@@ -10,13 +10,5 @@ describe('test', () => {
     chai.expect('ci with travis').to.equal('ci with travis');
   });
 
-  it('/user/login GET', function(done) {
-    this.timeout(0);
-    chai.request(app)
-      .get('/users/login')
-      .end(function(err, res){
-        expect(res.status).to.equal(200);
-        done();
-      });
-  });
+
 });
