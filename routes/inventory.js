5ebf272d4ff5bbd47d727482bc7b5121b1e11302
zchristianl <6568643+zchristianl@users.noreply.github.com>
2019-11-01 01:08:45 -0500

    New routes for inventory

--- routes/inventory.js
+++ routes/inventory.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const inventoryController = require('../controllers/inventoryController.js');
 
-router.post('/edit',inventoryController.inventory_modify);
-router.post('/create', inventoryController.inventory_modify);
-router.post('/delete', inventoryController.inventory_remove);
+router.get('/edit',inventoryController.inventory_modify);
+router.post('/edit/:id',inventoryController.inventory_modify);
+router.get('/create', inventoryController.inventory_modify);
+router.post('/create', inventoryController.inventory_create);
+router.post('/delete/:id', inventoryController.inventory_remove);
 router.get('/view', inventoryController.inventory_select);
 // Inventory home
 router.get('/', inventoryController.inventory_get);
