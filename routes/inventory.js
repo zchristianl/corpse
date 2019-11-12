e926770a3aeaee0ee2762e8142dfab6d3d9500c7
gregg1494 <gregg1494@gmail.com>
2019-11-12 03:11:11 -0600

    add route permissions and session timeout

--- routes/inventory.js
+++ routes/inventory.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const inventoryController = require('../controllers/inventoryController.js');
 
 //router.get('/edit',inventoryController.inventory_modify);
-router.get('/edit', global.ensureAuthenticated, inventoryController.inventory_modify);
-router.get('/edit/:id', global.ensureAuthenticated, inventoryController.inventory_modify);
-router.post('/edit', global.ensureAuthenticated, inventoryController.inventory_modify);
-router.get('/create', global.ensureAuthenticated, inventoryController.inventory_create);
-router.post('/create', global.ensureAuthenticated, inventoryController.inventory_create);
-router.post('/delete/:id', global.ensureAuthenticated, inventoryController.inventory_remove);
-router.get('/view', global.ensureAuthenticated, inventoryController.inventory_select);
+router.get('/edit', global.ensureSeller, inventoryController.inventory_modify);
+router.get('/edit/:id', global.ensureSeller, inventoryController.inventory_modify);
+router.post('/edit', global.ensureSeller, inventoryController.inventory_modify);
+router.get('/create', global.ensureSeller, inventoryController.inventory_create);
+router.post('/create', global.ensureSeller, inventoryController.inventory_create);
+router.post('/delete/:id', global.ensureSeller, inventoryController.inventory_remove);
+router.get('/view', global.ensureSeller, inventoryController.inventory_select);
 // Inventory home
-router.get('/', global.ensureAuthenticated, inventoryController.inventory_get);
+router.get('/', global.ensureSeller, inventoryController.inventory_get);
 
 module.exports = router;
