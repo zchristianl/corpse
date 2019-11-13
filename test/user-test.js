0c03148037412bbde8f92c86a585fca97751bc98
GIldons <raulgildons@gmail.com>
2019-11-13 21:59:02 -0200

    Add after login test
    Update chai-http package

--- test/user-test.js
+++ test/user-test.js
@@ -9,21 +9,21 @@ var expect = require('chai').expect;
 chai.use(chaiHttp);
 
 describe('user tests', () => {
-  it('/user/login GET', function(done) {
+  it('/user/login GET', function (done) {
     this.timeout(0);
     chai.request(app)
       .get('/users/login')
-      .end(function(err, res){
+      .end(function (err, res) {
         expect(res.status).to.equal(200);
         done();
       });
   });
 
-  it('/user/login View Test', function(done) {
+  it('/user/login View Test', function (done) {
     this.timeout(0);
     chai.request(app)
       .get('/users/login')
-      .end(function(err, res){        
+      .end(function (err, res) {
         expect(res.text).to.have.string('Login');
         expect(res.text).to.have.string('name=\"password\"');
         expect(res.text).to.have.string('type=\"password\"');
@@ -32,21 +32,21 @@ describe('user tests', () => {
       });
   });
 
-  it('/user/register GET', function(done) {
+  it('/user/register GET', function (done) {
     this.timeout(0);
     chai.request(app)
       .get('/users/register')
-      .end(function(err, res){
+      .end(function (err, res) {
         expect(res.status).to.equal(200);
         done();
       });
   });
 
-  it('/user/register View Test', function(done) {
+  it('/user/register View Test', function (done) {
     this.timeout(0);
     chai.request(app)
       .get('/users/register')
-      .end(function(err, res){
+      .end(function (err, res) {
         expect(res.text).to.have.string('<h1>Client Registration</h1>');
         expect(res.text).to.have.string('<input class="form-control" name="first_name" type="text" required="required" maxlength="25"');
         expect(res.text).to.have.string('<input class="form-control" name="email" type="email" required="required" maxlength="50"');
@@ -55,60 +55,65 @@ describe('user tests', () => {
       });
   });
 
-  it('/path POST test: login page connection', function(done) {
+  it('/path POST test: login page connection', function (done) {
     var path = '/users/login';
     chai
       .request(app)
       .post(path)
-    // .field('myparam' , 'test')
+      // .field('myparam' , 'test')
       .set('content-type', 'application/x-www-form-urlencoded')
-      .send({Email: 'test_email@mail.com', Password: 'asdf'})
-      .end(function(response) {
+      .send({ email: 'test_email@mail.com', password: 'asdf' })
+      .end(function (err, response) {
+        console.log(response)
         expect(response.status).to.equal(200);
         done();
       });
   });
 
-  it('/path POST test: login page data validation', function(done) {
+  var page_ = chai.request.agent(app)
+  it('/path POST test: login page data validation', function (done) {
     var path = '/users/login';
-    chai
-      .request(app)
+    page_
       .post(path)
-    // .field('myparam' , 'test')
       .set('content-type', 'application/x-www-form-urlencoded')
-      .send({Email: 'test_email@mail.com', Password: 'asdf'})
-      .end(function(response) {
+      .send({ email: 'duf@duf.com', password: 'duf' })
+      .end(function (err, response) {
         expect(response.accepted).to.be.equal(false);
         expect(response.serverError).to.be.equal(false);
         expect(response.clientError).to.be.equal(false);
         expect(response.error).to.be.equal(false);
-        done();
+        page_.get('/users/portal')
+          .end(function (err2, res2) {
+            console.log(res2)
+            done();
+          });
       });
+
   });
 
-  it('/path POST test: register page connection', function(done) {
+  it('/path POST test: register page connection', function (done) {
     var path = '/users/register';
     chai
       .request(app)
       .post(path)
-    // .field('myparam' , 'test')
+      // .field('myparam' , 'test')
       .set('content-type', 'application/x-www-form-urlencoded')
-      .send({Email: 'test_email@mail.com', Password: 'asdf'})
-      .end(function(response) {
+      .send({ Email: 'test_email@mail.com', Password: 'asdf' })
+      .end(function (err, response) {
         expect(response.status).to.equal(200);
         done();
       });
   });
 
-  it('/path POST test: register page data validation', function(done) {
+  it('/path POST test: register page data validation', function (done) {
     var path = '/users/register';
     chai
       .request(app)
       .post(path)
-    // .field('myparam' , 'test')
-    //  .set('content-type', 'application/x-www-form-urlencoded')
-    //  .send({Email: 'test_email@mail.com', Password: 'asdf'})
-      .end(function(response) {
+      // .field('myparam' , 'test')
+      //  .set('content-type', 'application/x-www-form-urlencoded')
+      //  .send({Email: 'test_email@mail.com', Password: 'asdf'})
+      .end(function (err, response) {
         expect(response.accepted).to.be.equal(false);
         expect(response.serverError).to.be.equal(false);
         expect(response.clientError).to.be.equal(false);
