50efac09dd285f68988f6cf51a523e8181b14c7d
gregg1494 <gregg1494@gmail.com>
2019-11-22 03:10:46 -0600

    client side problem

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -67,3 +67,32 @@ exports.payment_remove = (req, res) => {
   }).catch(err => logger.error(err));
   return res;
 };
+
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+
+async function create_session() {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [{
+      name: 'T-shirt',
+      description: 'Comfortable cotton t-shirt',
+      images: ['https://example.com/t-shirt.png'],
+      amount: 500,
+      currency: 'usd',
+      quantity: 1,
+    }],
+    success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+    cancel_url: 'https://example.com/cancel',
+  });
+  return session;
+}
+
+exports.checkout = (req, res) => {
+  create_session().then(session => {
+    console.log(session);
+    res.render('payment', {
+      session: session
+    });
+  });
+};
