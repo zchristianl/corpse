6a74b355ea88888dcae0b30a9fe1114608aab04b
zchristianl <6568643+zchristianl@users.noreply.github.com>
2019-11-25 13:40:36 -0600

    Gregg deleted the create payment route

--- routes/payment.js
+++ routes/payment.js
@@ -4,5 +4,6 @@ const paymentController = require('../controllers/paymentController');
 
 // checkout 
 router.get('/checkout', global.ensureAuthenticated, paymentController.checkout);
+router.post('/create', global.ensureAuthenticated, paymentController.payment_create);
 
 module.exports = router;
