82d0eb20d51c4b960118937524926d6eeb8b9443
gregg1494@gmail.com <gregg1494@gmail.com>
2019-11-29 19:53:02 -0600

    still working

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -4,11 +4,6 @@ require('dotenv').config();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-exports.checkout_get = (req, res) => {
-  console.log(req.params.id);
-  // create_session(req, res);
-};
-
 exports.payment_get = (req, res) => {
   models.Payment.findAll({
 
@@ -47,51 +42,57 @@ exports.payment_remove = (req, res) => {
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
 
-async function create_session(req, res) {
-  console.log(req.params.id);
-  let checkout_items = [];
-  // Build an item list of all items in an order
+function create_session(req, res) {
+  // array of items for stripe checkout
+  let checkout_items = new Array();
+  // Find all items with this orderId
   models.Item.findOne({
     where: {
       orderId: req.params.id
     }
-  }).then(items => {
-    for(const item in items) {
-      models.Inventory.findOne({
-        where: {
-          id: item.inventoryId
-        }
-      }).then(inventory_item => {
-        let checkout_item = {
-          name: inventory_item.name,
-          description: inventory_item.description,
-          amount: inventory_item.price * 100,
-          currency: 'usd',
-          quantity: 1,
-        };
-        checkout_items.push(checkout_item);
+  }).then(item => {
+    // look through each item and get info from inventory
+    models.Inventory.findOne({
+      where: {
+        id: item.inventoryId
+      }
+    }).then(inventory_item => {
+      // create an object for stripe checkout
+      let checkout_item = {
+        name: inventory_item.name,
+        description: inventory_item.description,
+        amount: inventory_item.price * 100,
+        currency: 'usd',
+        quantity: 1,
+      };
+      // add object to array for checkout
+      checkout_items.push(checkout_item);
+      return checkout_items;
+    }).then(checkout_items => {
+      console.log(checkout_items);
+      stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: checkout_items,
+        success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url: 'https://example.com/cancel',
+      }).then(session => {
+        return session;
       });
-    }
-    console.log(checkout_items);
-    // Create session with list
-    const session = stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: checkout_items,
-      success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-      cancel_url: 'https://example.com/cancel',
+    }).catch(err => {
+      logger.error(err);
     });
-    return session;  
-  }).catch(err => {
-    logger.error(err);
-    req.flash('danger', 'Error when try to pay!');
-    res.redirect('client-dashboard');
   });
 }
 
 exports.checkout = (req, res) => {
-  create_session().then(session => {
+  const session = create_session(req, res);
+  if(session) {
     res.render('payment', {
       session: session
     });
-  });
+  }
+  else {
+    req.flash('danger', 'Error when trying to pay!');
+    res.redirect('client-dashboard');
+  }
 };
