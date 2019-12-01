de4c55e69dcdc9961ef34834a09901420fa9dcf2
gregg1494@gmail.com <gregg1494@gmail.com>
2019-12-01 13:42:44 -0600

    simplify payment call, add documentation

--- routes/payment.js
+++ routes/payment.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const paymentController = require('../controllers/paymentController');
 
 // checkout 
-router.get('/checkout/:id', global.ensureAuthenticated, paymentController.checkout);
+router.get('/checkout/:id', global.ensureAuthenticated, paymentController.create_session);
 router.post('/create', global.ensureAuthenticated, paymentController.payment_create);
 
 router.post('/payment_complete', bodyParser.raw({type: 'application/json'}), paymentController.stripe_webhook); 
